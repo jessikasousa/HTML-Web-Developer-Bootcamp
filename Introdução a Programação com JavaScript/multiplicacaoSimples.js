c0f023e9a61a83b0fd761f57b98650eed8cc41a9
Jéssika Sousa <84594383+jessikasousa@users.noreply.github.com>
2021-07-07 20:37:17 -0300

    Add files via upload

---
+++ Introdução a Programação com JavaScript/multiplicacaoSimples.js
@@ -0,0 +1,8 @@
+/* Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   */
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado
+
+console.log("PROD = " + total);
